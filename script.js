d52fde9981ad6152823e2a550d59e1b6875300f8
Najibat <najibatamubikanhun@gmail.com>
2025-01-15 13:41:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Technical Writer", "Blockchain Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    BackDelay: 1000,
+    loop: true,
+});
+});
+
